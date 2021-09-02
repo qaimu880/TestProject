a85bd6795143ce9ca754a6751851bea64d6fa3d4
qaimu880 <qaiser.munir@hotmail.com>
2021-09-02 12:21:05 +0500

    testcase2

---
+++ A1/src/test/java/TestProject/A1/TestCase2.java
@@ -0,0 +1,14 @@
+package TestProject.A1;
+
+import org.testng.annotations.Test;
+
+public class TestCase2 
+{
+	@Test
+	public void TestMethod2()
+	{
+		System.out.println(" ****HelloWorld Print Successfully****");
+		System.out.println(" ****HelloWorld Print Successfully*****");
+		System.out.println(" ****HelloWorld Print Successfully*****");
+	}
+}
