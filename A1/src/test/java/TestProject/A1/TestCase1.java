5e1be7d6063e236ce3fecc33ea483611f9735322
qaimu880 <qaiser.munir@hotmail.com>
2021-09-02 11:23:01 +0500

    first commit

---
+++ A1/src/test/java/TestProject/A1/TestCase1.java
@@ -0,0 +1,13 @@
+package TestProject.A1;
+
+import org.testng.annotations.Test;
+
+
+public class TestCase1 
+{
+@Test
+	public void TestMethod1()
+	{
+		System.out.println(" ****HelloWorld Print Successfully****");
+	}
+}
