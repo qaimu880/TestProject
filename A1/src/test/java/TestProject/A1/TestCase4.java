6da88c4306f7ff461d51ca75831f16629f67e3ce
qaiser.munir <qaiser.munir@DESKTOP-2KTVUEB.d-p-s.com>
2021-09-03 10:07:39 +0500

    added class testcase4

---
+++ A1/src/test/java/TestProject/A1/TestCase4.java
@@ -0,0 +1,13 @@
+package TestProject.A1;
+
+import org.testng.annotations.Test;
+
+public class TestCase4 
+{
+
+	@Test
+	public void testmethod4()
+	{
+		System.out.println("this is test method4 output");
+	}
+}
